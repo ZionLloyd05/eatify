f050590cb6a5ed9604086ba2ebd806bb61075cb1
ZionLloyd05 <zionlloyd05@gmail.com>
2019-09-16 09:44:26 +0100

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,15 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant Update(Restaurant updatedRestaurant);
+        int Commit();
+        Restaurant GetById(int id);
+        Restaurant Add(Restaurant newRestaurant);
+    }
+}
